38638ef2fbfc25abe10319046020bfd0f8701c13
m4tei4real-eng <m4tei4real@gmail.com>
2026-05-12 01:27:51 -0700

    Add files via upload

---
+++ event2.js
@@ -0,0 +1,19 @@
+let x=document.getElementById("boyka")
+let -=document.getElementById("boy")
+let z=document.getElementById("cat")
+let a=document.getElementById("dog")
+
+x.addEventListener("click",function(){
+    z.style.backgroundColor="yellow"
+})
+y.addEventListener("click",function(){
+    a.style.backgroundColor="green"
+})
+
+
+let c=document.getElementById("girl")
+let b=document.getElementById("cow")
+c.addEventListener("dblclick",function(){
+    b.style.backgroundColor="yellow"
+})
+
